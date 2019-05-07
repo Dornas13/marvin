1d60e689488bb3b95696805381f42a1ba00af9a3
Dornas13 <44907726+Dornas13@users.noreply.github.com>
2019-05-07 20:42:46 -0300

    console.log (vogalOuConsoante("a"))

---
+++ fase04/ex03/vogalOuConsoante.js
@@ -0,0 +1,15 @@
+function vogalOuConsoante(x) {
+ if (x === "A") {return "Vogal"} 
+ if (x === "E") {return "Vogal"}
+ if (x === "I") {return "Vogal"}
+ if (x === "O") {return "Vogal"}
+ if (x === "U") {return "Vogal"} 
+ if (x === "a") {return "Vogal"} 
+ if (x === "e") {return "Vogal"}
+ if (x === "i") {return "Vogal"}
+ if (x === "o") {return "Vogal"}
+ if (x === "u") {return "Vogal"} 
+ 
+   
+    else {return("Consoante")}
+}
