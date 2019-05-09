66fdd7907fa843b565d3ec6325050bef0cfc88e3
Dornas13 <44907726+Dornas13@users.noreply.github.com>
2019-05-09 19:47:34 -0300

    console.log(parOuImpar(56651511))

--- fase04/ex00/parOuImpar.js
+++ fase04/ex00/parOuImpar.js
@@ -1,12 +1,4 @@
 function parOuImpar(y) {
-
- var x = parseInt(y.toString().charAt( y.length - 1 ))
- 
- if (x === 0) {return "Par"}
- if (x === 2) {return "Par"}
- if (x === 4) {return "Par"}
- if (x === 6) {return "Par"}
- if (x === 8) {return "Par"}
- else {return "Impar"}
- 
+    if (y % 2 === 0) {return "Par"}
+    else {return "Ímpar"} 
 }
