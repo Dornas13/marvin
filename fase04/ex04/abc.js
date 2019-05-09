31662e6304ef5db44ce00eefbc2277653b99894b
Dornas13 <44907726+Dornas13@users.noreply.github.com>
2019-05-09 20:03:36 -0300

    Update abc.js

--- fase04/ex04/abc.js
+++ fase04/ex04/abc.js
@@ -1,12 +1,12 @@
 function abc(x) {
- if (x.substring(0,1) === "A") {return "true"} 
- if (x.substring(0,1) === "B") {return "true"}
- if (x.substring(0,1) === "C") {return "true"}
- if (x.substring(0,1) === "c") {return "true"}
- if (x.substring(0,1) === "b") {return "true"} 
- if (x.substring(0,1) === "a") {return "true"} 
+ if (x.substring(0,1) === "A") {return true} 
+ if (x.substring(0,1) === "B") {return true}
+ if (x.substring(0,1) === "C") {return true}
+ if (x.substring(0,1) === "c") {return true}
+ if (x.substring(0,1) === "b") {return true} 
+ if (x.substring(0,1) === "a") {return true} 
 
  
    
-    else {return("false")}
+    else {return(false)}
 }
