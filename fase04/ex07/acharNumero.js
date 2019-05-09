7fc48fafeb5305f39919af580b24cbf69e19482d
Dornas13 <44907726+Dornas13@users.noreply.github.com>
2019-05-09 20:17:38 -0300

    Update acharNumero.js

--- fase04/ex07/acharNumero.js
+++ fase04/ex07/acharNumero.js
@@ -1,7 +1,7 @@
 function acharNumero(x) {
  if (x<5) {return("a")}
- if (x>4.99999999999999999999 && x<10.000000000000000001) {return("b")}
- if (x>9.9999999999999999999 && x<100.00000000000000001) {return("c")}
+ if (x>=5 && x<=10) {return("b")}
+ if (x>10 && x<=100) {return("c")}
  else {return("d")}
  
 }
