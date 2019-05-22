926f74fe301b61178dba9e0265307e61c06a008e
Dornas13 <44907726+Dornas13@users.noreply.github.com>
2019-05-22 19:40:58 -0300

    console.log(ateZero(12))

---
+++ fase06/ex01/ateZero.js
@@ -0,0 +1,16 @@
+function ateZero (x) {
+    var z = []
+    
+    while(x<0){
+        z.push(x)
+        x++
+    }
+
+    var y = 0
+    
+    while(x>=y){
+        z.push(y)
+        y++
+    }
+return z
+}
