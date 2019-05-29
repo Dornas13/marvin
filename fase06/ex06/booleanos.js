37346e5e4e2484487f05df092bd96cbdc2565349
Dornas13 <44907726+Dornas13@users.noreply.github.com>
2019-05-29 20:48:08 -0300

    console.log(booleanos([true, 1, -10, "carro", false, 0]))

---
+++ fase06/ex06/booleanos.js
@@ -0,0 +1,14 @@
+function booleanos (x) {
+a = []
+i = 0
+u = 0
+    while (x.length > i){
+     if (typeof x  [i] != "boolean"){
+         a[u] = x[i]
+         u++
+    }    
+    i++
+}
+return a
+}
+
